ce098b166881ce2125593ad1e6f3da33c32f2010
manudana11 <manueldanapallardo@gmail.com>
2024-05-26 23:10:52 +0200

    hola

--- src/components/ListNews/ListNews.jsx
+++ src/components/ListNews/ListNews.jsx
@@ -19,9 +19,8 @@ const ListNews = () => {
         return (
             <div key={notice.id} className='list-news-item'>
                 {imageUrl && <img src={imageUrl} alt={notice.title} />}<h2>{notice.title}</h2>
-                <p><strong>By:</strong> {notice.byline}</p>
+                <p>{notice.byline}</p>
                 <p><strong>Published:</strong> {notice.published_date}</p>
-                <p><strong>Updated:</strong> {notice.updated}</p>
                 <p><strong>Source:</strong> {notice.source}</p>
                 <p><strong>Section:</strong> {notice.section}</p>
                 
